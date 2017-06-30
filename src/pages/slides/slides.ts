efa369d5fcfc70ac2d9901b9bc729878c31e5e52
Viachesalv Osadchiy <wavemeup1@gmail.com>
2017-06-30 10:56:48 +0300

    изображения для слайдов

--- src/pages/slides/slides.ts
+++ src/pages/slides/slides.ts
@@ -13,27 +13,27 @@ export class SlidesPage {
     {
       title: "Добро пожаловать!",
       description: "<b>Fintech App</b> предназначено для систематизации финансовых взаиморасчетов между людьми.",
-      image: "assets/img/dialog.png",
+      image: "assets/img/slides/dialog.png",
     },
     {
       title: "Добавьте собеседника",
       description: "На экране поиска введите <b>номер абонента</b>.",
-      image: "assets/img/search.png",
+      image: "assets/img/slides/search.png",
     },
     {
       title: "Изначальный баланс",
       description: "Вы можете начать диалог с <b>любым</b> балансом, даже отрицательным.",
-      image: "assets/img/balance.png",
+      image: "assets/img/slides/balance.png",
     },
     {
       title: "Тип транзакции",
       description: "Выберите тип рассчета, <b>наличый или безнал</b>. В случае наличного рассчета потребуется подтверждение от получателя.",
-      image: "assets/img/type.png",
+      image: "assets/img/slides/type.png",
     },
     {
       title: "Отправка",
       description: "Введите сумму, и, при необходимости, комментарий, и <b>всё готово к отправке!</b>",
-      image: "assets/img/send.png",
+      image: "assets/img/slides/send.png",
     }
   ];
 
